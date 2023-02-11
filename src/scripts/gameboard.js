5742cff831c27feb35c94e9ba604d45fda43ce7c
Adzi <belkoadam@gmail.com>
2023-02-11 13:29:52 +1100

    Validation for adding ships on a gameboard in place

--- src/scripts/gameboard.js
+++ src/scripts/gameboard.js
@@ -1,5 +1,3 @@
-// import Ship from "./ship.js";
-
 const Gameboard = () => {
     // Create a board 10 x 10
     let board = Array(10)
@@ -22,16 +20,47 @@ const Gameboard = () => {
         return [hor, ver];
     };
 
+    // Check if ship is placed within the gameboard squares
+    const checkShipPlacement = (x, y, length, direction) => {
+        for (let i = 0; i < length; i++) {
+            if (direction === "hor" && x + length < 10) {
+                return true;
+            } else if (direction === "ver" && y + length < 10) {
+                return true;
+            } else {
+                return false;
+            }
+        }
+    };
+
+    // Check if ship collides with another ship
+    const checkShipCollision = (x, y, length, direction) => {
+        for (let i = 0; i < length; i++) {
+            const [hor, ver] = getCoords(x, y, i, direction);
+
+            if (board[hor][ver] !== null || board[hor][ver] !== null) {
+                return false;
+            }
+        }
+        return true;
+    };
+
     const placeShip = (ship, x, y) => {
         const direction = ship.getDirection();
+        const length = ship.getLength();
+        const valid1 = checkShipCollision(x, y, length, direction);
+        const valid2 = checkShipPlacement(x, y, length, direction);
 
-        for (let i = 0; i < ship.getLength(); i++) {
+        if (!valid1) return;
+        if (!valid2) return;
+
+        for (let i = 0; i < length; i++) {
             const [hor, ver] = getCoords(x, y, i, direction);
             board[hor][ver] = "ship";
         }
     };
 
-    return { getBoard, placeShip };
+    return { getBoard, checkShipPlacement, checkShipCollision, placeShip };
 };
 
 export default Gameboard;
--- src/scripts/tests/gameboard.test.js
+++ src/scripts/tests/gameboard.test.js
@@ -4,10 +4,10 @@ import Ship from "../ship.js";
 
 describe("Gameboard factory", () => {
     test("Place ship horizontally", () => {
-        const player = Gameboard();
-        const myShip = Ship(4);
-        player.placeShip(myShip, 0, 2);
-        const shipPos = player.getBoard();
+        const board = Gameboard();
+        const myShip = Ship(3);
+        board.placeShip(myShip, 0, 2);
+        const shipPos = board.getBoard();
         for (let i = 0; i < myShip.getLength(); i++) {
             expect(shipPos[i][2]).toBe("ship");
         }
@@ -15,15 +15,31 @@ describe("Gameboard factory", () => {
     });
 
     test("Place ship vertically", () => {
-        const player = Gameboard();
+        const board = Gameboard();
         const myShip = Ship(3);
         myShip.changeDirection();
-        player.placeShip(myShip, 3, 2);
-        const shipPos = player.getBoard();
+        board.placeShip(myShip, 3, 2);
+        const shipPos = board.getBoard();
         for (let i = 2; i < myShip.getLength(); i++) {
             expect(shipPos[3][i]).toBe("ship");
         }
         expect(shipPos[3][1]).toBe(null);
         expect(shipPos[3][6]).toBe(null);
+    });
+
+    test("Check if ship is placed within the board", () => {
+        const board = Gameboard();
+        // checkShipPlacement(x, y, length, direction)
+        expect(board.checkShipPlacement(7, 0, 3, "hor")).toBeFalsy();
+        expect(board.checkShipPlacement(6, 0, 3, "hor")).toBeTruthy();
+    });
+
+    test("Check if ship collides whith another ship", () => {
+        const board = Gameboard();
+        const myShip = Ship(4);
+        board.placeShip(myShip, 0, 1);
+        // checkShipPlacement(x, y, length, direction)
+        expect(board.checkShipCollision(0, 1, 4, "hor")).toBeFalsy();
+        expect(board.checkShipCollision(5, 1, 4, "hor")).toBeTruthy();
     });
 });
